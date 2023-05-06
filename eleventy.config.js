50f799cdb54e4c7c5e4a98674bb2a1ce339a865c
Samuel Hoskin <hoskin2012@gmail.com>
2023-05-06 21:52:38 +0100

    Update Favicon to Emoji

--- eleventy.config.js
+++ eleventy.config.js
@@ -10,6 +10,13 @@ module.exports = function(eleventyConfig) {
     },
     fontFamily: 'arial',
 
+    icons: {
+      mask: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝 </text></svg>',
+      shortcut: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝 </text></svg>',
+      touch: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝 </text></svg>',
+
+    },
+
     footer:{
       contentLicence: {
         text: ' '
